7f14cab659dd882c75f1644d71f15138ed6c62e9
Abi Hafshin <abi@hafs.in>
2026-01-04 13:23:14 +0700

    error when at least one update failed

--- update-all.sh
+++ update-all.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+fail=0
 for update_file in */update.sh; do
   echo "::group::Running update script: $update_file"
-  bash "$update_file" || true
+  if ! bash "$update_file"; then
+    echo "❌ Update script failed"
+    fail=1
+  fi
   echo "::endgroup::"
 done
+
+if [ "$fail" -ne 0 ]; then
+  echo "One or more update scripts failed."
+  exit 1
+fi
