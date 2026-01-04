8132a34ed1d6302e5cc8364c9bd592dc167eb079
Abi Hafshin <abi@hafs.in>
2026-01-04 13:20:37 +0700

    ignore failed update

--- update-all.sh
+++ update-all.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 for update_file in */update.sh; do
   echo "::group::Running update script: $update_file"
-  bash "$update_file"
+  bash "$update_file" || true
   echo "::endgroup::"
 done
