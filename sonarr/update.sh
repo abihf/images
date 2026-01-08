45d276488d7821aa9ace7ac53c261bf9f5b33e23
Abi Hafshin <abi@hafs.in>
2026-01-08 17:41:38 +0700

    sonarr v5

--- sonarr/update.sh
+++ sonarr/update.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 source "$(dirname "$0")/../utils.sh"
 
 SONARR_REPO="Sonarr/Sonarr"
-SONARR_VERSION=$(get_latest_github_release_tag "$SONARR_REPO" | sed 's/^v//')
-update_docker_arg "SONARR_VERSION" "${SONARR_VERSION}"
-echo "Latest Sonarr version: $SONARR_VERSION"
+SONARR_BRANCH="v5-develop"
+SONARR_COMMIT=$(get_latest_github_commit "$SONARR_REPO" "$SONARR_BRANCH")
+SONARR_COMMIT_COUNT=$(get_number_of_commits "$SONARR_REPO" "$SONARR_COMMIT")
+update_docker_arg "SONARR_COMMIT" "${SONARR_COMMIT}"
+update_docker_arg "SONARR_COMMIT_COUNT" "${SONARR_COMMIT_COUNT}"
+update_docker_arg "SONARR_BRANCH" "${SONARR_BRANCH}"
+echo "Latest Sonarr commit: $SONARR_COMMIT ($SONARR_COMMIT_COUNT) on $SONARR_REPO ($SONARR_BRANCH)"
--- utils.sh
+++ utils.sh
@@ -23,6 +23,16 @@ function get_latest_github_release_tag() {
     jq -r '.[0].tag_name'
 }
 
+function get_number_of_commits() {
+  local repo="$1"
+  local branch="${2:-main}"
+
+  curl -sIL \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/${repo}/commits?per_page=1&sha=${branch}" | grep -i '^Link:' | sed -E 's/^.*&page=([0-9]+)>; rel="last".*$/\1/'
+}
+
 function update_docker_arg() {
   local arg_name="$1"
   local new_value="$2"
