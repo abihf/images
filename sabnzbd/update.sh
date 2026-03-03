b723f038b98076769c79903f4030585e8ef02397
Abi Hafshin <abi@hafs.in>
2026-03-03 18:58:51 +0700

    sabnzbd: auto update unrar

--- sabnzbd/update.sh
+++ sabnzbd/update.sh
@@ -9,6 +9,11 @@ PAR2_COMMIT=$(get_latest_github_commit "$PAR2_REPO" "$PAR2_BRANCH")
 update_docker_arg "PAR2_COMMIT" "$PAR2_COMMIT"
 echo "Latest par2cmdline-turbo commit on $PAR2_REPO ($PAR2_BRANCH): $PAR2_COMMIT"
 
+UNRAR_REPO="EDM115/unrar-alpine"
+UNRAR_VERSION=$(get_latest_github_release_tag "$UNRAR_REPO")
+update_docker_arg "UNRAR_VERSION" "$UNRAR_VERSION"
+echo "Latest unrar-alpine release on $UNRAR_REPO: $UNRAR_VERSION"
+
 SABNZBD_REPO="sabnzbd/sabnzbd"
 SABNZBD_BRANCH="develop"
 SABNZBD_COMMIT=$(get_latest_github_commit "$SABNZBD_REPO" "$SABNZBD_BRANCH")
